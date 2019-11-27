4fc774b5a4367efae83bccb0272946c2bdf3631f
Luis <luis901101@gmail.com>
2019-11-26 19:39:27 -0500

    - minor warnings fixed - v0.0.4 uploaded to pub.dev

--- lib/scanner_callback.dart
+++ lib/scanner_callback.dart
@@ -1,21 +1,11 @@
-import 'package:meta/meta.dart';
-
-/**
- * Callback of the decoding process
- */
+ /// Callback of the decoding process
 abstract class ScannerCallBack {
-  /**
-   * Called when decoder has successfully decoded the code
-   * <br>
-   *
-   * @param result Encapsulates the result of decoding a barcode within an image
-   */
-  void onDecoded(@required String result);
-  /**
-   * Called when error has occurred
-   * <br>
-   *
-   * @param error Exception that has been thrown
-   */
-  void onError(@required Exception error);
+   /// Called when decoder has successfully decoded the code
+   /// <br>
+   /// @param result Encapsulates the result of decoding a barcode within an image
+  void onDecoded(String result);
+   /// Called when error has occurred
+   /// <br>
+   /// @param error Exception that has been thrown
+  void onError(Exception error);
 }
